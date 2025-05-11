6047be2d527744e2548e2e99bfaca10dbf4b49e9
KentseMatshira <154395251+KentseMatshira@users.noreply.github.com>
2025-05-11 21:21:35 +0200

    Added A check on loyalty if true add a star next to reveiw if not leave empty

--- src/index.ts
+++ src/index.ts
@@ -20,14 +20,8 @@ const reviews = [
     date: "27-03-2021",
   },
 ];
-function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
-  const iconDisplay = isLoyalty ? "⭐" : "";
-  reviewTotalDisplay.innerHTML =
-    "review total " +
-    value.toString() +
-    "| last reviewed by " +
-    reviewer +
-    " " +
-    iconDisplay;
+function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean) {
+  const iconDisplay = isLoyalty ? '⭐' : ''
+  reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + iconDisplay
 }
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
